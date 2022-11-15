3d464c202ebd51dd3680e48e0c0f78cd6feefc7d
Charan Venkat Chandran <chandran7824@students.d211.org>
2022-11-15 09:30:07 -0600

    sQ
    qs

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const acorn = require('acorn')
+const { parse } = require('@babel/parser')
+const traverse = require('@babel/traverse')
 const fs = require('fs')
-const cool = require('./test')
-console.log(cool)
-console.dir(cool('div', { id: 'foo' }, cool('a', null, 'bar'), cool('b')), { depth: null })
-const test = fs.readFileSync('test.js').toString()
-console.log(acorn.parse(test, { ecmaVersion: 2020 }))
 
-//https://www.one-tab.com/page/h_FA0DAhQqCMD_CbDpvWOg
+const file = fs.readFileSync('./index.js').toString()
+const ast = parse(file)
+
+traverse(ast, {
+    ExpressionStatement: node => {
+        console.log(node)
+    }
+})
